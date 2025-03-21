30aaf7107f0b0bf888cd8c93c2b71899b89bd1c8
giovanaas <azevedogiovana309@gmail.com>
2025-03-21 14:19:11 -0300

    exercicio2

---
+++ Aula4/exercicio2/exericio2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+int numA, numB; {
+    cout << "Dgite o primeiro numero INTEIRO: ";
+    cin >> numA;
+    cout << "Dgite o primeiro numero INTEIRO: ";
+    cin >> numB;
+} if (numA % numB==0 || numB % numA==0 ) {
+    cout << "os numeros " << numA << " e " << numB << " sao multiplos";
+} else {
+    cout << "os numeros " << numA << " e " << numB << " nao sao multiplos";
+}
+} 
