9350eda2b46981b9601c8c4943a2868a47b30c62
giovanaas <azevedogiovana309@gmail.com>
2025-03-21 10:44:30 -0300

    tarefas

---
+++ Aula4/exercicio1/exercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.Win32.SafeHandles;
+
+Console.WriteLine ("Digite um número INTEIRO: ");
+int numero= Convert.ToInt32 (Console.ReadLine ());
+
+ if (numero %2==0) {
+ Console.WriteLine ($"O número {numero} é par.");
+ } else {
+     Console.WriteLine ($"O número {numero} é ímpar.");
+ }
