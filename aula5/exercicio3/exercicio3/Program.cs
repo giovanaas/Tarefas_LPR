6cca5ce1855a27752a3fb2f213f00fccf6883d8b
giovanaas <azevedogiovana309@gmail.com>
2025-03-28 13:06:51 -0300

    exercicio3

---
+++ aula5/exercicio3/exercicio3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+{ 
+    int soma = 0;
+
+    for (int i=50; i<= 500; i++) {
+        if (i % 2 != 0 && i % 3 == 0 ) {
+
+       soma += i;
+        }
+    }
+Console.WriteLine ($"A soma de todos os números ímpres múltiplos de 3 que se encontram de 50 a 500 é: {soma} ");
+
+    }
+}
