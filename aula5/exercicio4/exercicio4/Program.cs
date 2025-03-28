dff7fdc684375121fd0216732917cbfd9563fc22
giovanaas <azevedogiovana309@gmail.com>
2025-03-28 13:56:26 -0300

    exercicio4

---
+++ aula5/exercicio4/exercicio4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Digite um número");
+ 
+int numero= Convert.ToInt32 (Console.ReadLine ());
+int quadrado = numero*numero;
+int soma = 0;
+
+string coisa = quadrado.ToString();
+
+foreach (char Num in coisa)
+{
+    soma += Num-48;
+} 
+Console.WriteLine ($"a soma dos dígitos do quadrado desse número é {soma}");
