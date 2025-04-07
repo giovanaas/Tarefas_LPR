84c4517cd3108181b432ccba89df2c2e73176e5e
giovanaas <azevedogiovana309@gmail.com>
2025-04-07 11:29:36 -0300

    numReverso

---
+++ Aula6/exercicio1/exercicio1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{  
+    int numero;
+    cout << "Digite um numero inteiro: ";
+    cin >> numero;
+    cout << "O numero eh: " << numero << ".";
+
+}
