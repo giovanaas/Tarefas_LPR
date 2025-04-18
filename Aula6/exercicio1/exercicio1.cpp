69dd01570ec04d5fd34b143b78fa8bc81cc1919c
giovanaas <azevedogiovana309@gmail.com>
2025-04-18 10:06:20 -0300

    exercicio1

--- Aula6/exercicio1/exercicio1.cpp
+++ Aula6/exercicio1/exercicio1.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
-int main()
-{  
-    int numero;
-    cout << "Digite um numero inteiro: ";
-    cin >> numero;
-    cout << "O numero eh: " << numero << ".";
+int revertamento(int numero)
+{
+    int reverse = 0;
 
+    while (numero != 0)
+    {
+        int digito = numero % 10;
+        reverse = reverse * 10 + digito;
+        numero /= 10;
+    }
+
+    return reverse;
+}
+
+int main()
+{
+    int numero;
+    string entrada;
+
+    while (true)
+    {
+        cout << "Digite um numero INTEIRO: ";
+
+        if (cin >> numero)
+        {
+            int resultado = revertamento(numero);
+            cout << "O reverso de " << numero << " eh: " << resultado << "." << endl;
+            cout << " " << endl;
+        }
+        else
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            cout << "Por favor, digite um numero valido! O numero tem que ser INTEIRO!!" << endl;
+            cout << " " << endl;
+        }
+    }
+    return 0;
 }
