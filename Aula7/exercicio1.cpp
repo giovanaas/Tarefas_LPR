84c5cb193e2a9d48deaeb46c4e27a63bf111d052
giovanaas <azevedogiovana309@gmail.com>
2025-06-02 11:00:04 -0300

    aula7

---
+++ Aula7/exercicio1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int numeros[10];
+   
+    cout << "Digite DEZ numeros inteiros, um por vez: ";
+    
+    int inteiros=0;
+
+    while (inteiros<10) {
+       inteiros++;
+    }
+
+}
