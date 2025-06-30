302c50f8cc442d54e4f65ecd1dfd1bc58928b107
giovanaas <azevedogiovana309@gmail.com>
2025-06-30 10:46:00 -0300

    Aula9

---
+++ Aula9/exercico1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void main()
+    {
+        List<string> nomesAleatorios = new();
+
+        Console.WriteLine("Quantos nomes você deseja ordenar?");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i <= quantidade; i++)
+        {
+            Console.WriteLine($" {i}º nome: ");
+            string nome = Console.ReadLine();
+        }
+        
+
+    }
+}
