30e90a09cfc760b5c3f2231f2722bb303d66b857
giovanaas <azevedogiovana309@gmail.com>
2025-07-13 14:37:41 -0300

    tarefasLPR

--- Aula4/exercicio1/exercicio1/Program.cs
+++ Aula4/exercicio1/exercicio1/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.Win32.SafeHandles;
 
-Console.WriteLine ("Digite um número INTEIRO: ");
+Console.WriteLine ("Digite um número inteiro: ");
 int numero= Convert.ToInt32 (Console.ReadLine ());
 
  if (numero %2==0) {
--- Aula6/exercicio1/exercicio1.cpp
+++ Aula6/exercicio1/exercicio1.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <locale.h>
 using namespace std;
   
 int numero;
 int reverso;
 
 void reverso () {
-  
+
+    setlocale(LC_ALL, "pt_BR.UTF-8");  // ver como que faz isso aqui direito
     cout << "Digite um numero inteiro: ";
     cin >> numero;
 }
